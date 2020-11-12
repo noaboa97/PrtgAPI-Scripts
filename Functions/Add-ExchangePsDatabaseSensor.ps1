d7105ace9936e4cb9ca5ac77c7016fc8a24a93f7
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-11-12 18:47:25 +0100

    Create Add-ExchangePsDatabaseSensor.ps1
    Initial

---
+++ Functions/Add-ExchangePsDatabaseSensor.ps1
@@ -0,0 +1,79 @@
+function Add-ExchangePsDatabaseSensor{
+    Param (
+            [parameter(valuefrompipeline = $true, HelpMessage = "PRTG Device Object", Position = 0)]
+            [PrtgAPI.Device]$device,
+            [parameter(valuefrompipeline = $true, HelpMessage = "Enter logfile path", Position = 1)]
+            [string]$logfile = "C:\Scripts\PRTG\Logs\LOG_Add-ExchangePsDatabaseSensor.txt"
+        )
+
+    # TO DO:
+    # - Test the new features
+    # - make settings customizable
+    # - make removal of voltages, other and currents sensor optional
+    # - Synopsis
+    # - Comment code
+
+    If (!(Test-Path $logfile)) {New-Item -Path $logfile -Force}
+       
+
+    if($logfile -eq "C:\Scripts\PRTG\Logs\LOG_Add-ExchangePsDatabaseSensor.txt"){
+        Add-content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): -------------------------------Adding PS Exchange DB sensor to $($device.name)-------------------------------"
+    }
+    else{
+
+        Add-content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): -------------------------------PS Exchange DB sensor-------------------------------"
+
+    }
+
+    #Get the interfaces of the device which are dynamic per device
+    Write-Host "Loading targets"
+    Add-content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): Loading targets"
+    try{
+        $targets = $device | get-sensortarget -rawType exchangepsdatabase -Timeout 5000 -ErrorAction Stop 
+    }
+    catch{
+    
+        Add-content -path $logfile -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): $($_)"
+        return
+
+    }
+
+    if($targets -eq $null){
+    
+        Write-Host "No targets found"
+        Add-Content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): No targets found"
+        return
+    
+    }
+
+    $i = 0
+    Write-Host "Creating Sensors"
+    Add-content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): Creating Sensors"
+    foreach($target in $targets){
+
+        Add-content -path $logfile  -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): Creating sensor for Exchange DB $($targets[$i].name)"
+        $target = $targets[$i].properties -join "|"
+        $i ++
+
+        $table = @{
+            "name_"="Base Sensor"
+            "tags_"="exchange powershell database"
+            "priority_"=3
+            "elementlist_"=1
+            "elementlist__check"=$target
+            "remount"=0
+            "writeresult"=0
+            "intervalgroup"=0
+            "interval_"="300|5 minutes"
+            "errorintervalsdown_"=1
+            "inherittriggers"=1
+            "id"=$device.id
+            "sensortype"="exchangepsdatabase"
+        }
+
+        $params = New-SensorParameters $table
+
+        Get-Device -id $device.id | Add-Sensor $params
+    }
+
+}
